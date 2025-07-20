9009f68d9391e815fb170ed82f3e16ff882d05a3
punkushal <spycymagar@gmail.com>
2025-07-20 08:46:49 +0545

    custom textfield widget added

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.labelText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+      ),
+      validator: (value) {
+        if (value!.isEmpty || value == "") {
+          return "Please enter the title";
+        } else if (value.length < 4) {
+          return "Please enter title length greater than 4";
+        }
+        return null;
+      },
+    );
+  }
+}
